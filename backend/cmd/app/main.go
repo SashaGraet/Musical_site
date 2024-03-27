15ee4eac4a9a2c726e1417c88c1dc9bc8da85cfe
Sanya <Lemon307@mail.ru>
2024-03-28 03:33:40 +0700

    another version

---
+++ backend/cmd/app/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"backend/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	// TODO: init storage: PostgreSQL
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+
+	return log
+
+}
