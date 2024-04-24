a7bc86b6b07b1d23f2df6613e8fa027781ca8ded
Sanya <Lemon307@mail.ru>
2024-04-24 20:07:46 +0700

    added remote db

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -4,12 +4,18 @@ import (
 	"backend/internal/database"
 	"backend/internal/routes"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
